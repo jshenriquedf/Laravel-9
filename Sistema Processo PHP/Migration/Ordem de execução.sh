86fc110f1b26dfef76bf9aab9406fbcbd5832270
jshenriquedf <68820356+jshenriquedf@users.noreply.github.com>
2022-03-23 23:22:02 -0300

    Update Ordem de execução.sh

--- Sistema Processo PHP/Migration/Ordem de execução.sh
+++ Sistema Processo PHP/Migration/Ordem de execução.sh
@@ -1,24 +1,17 @@
 # Criação do arquivo de ordem para execução
 
 # Criação de arquivos migration
-php artisan make:migration create_table_uf --path=database/migrations/admin
-php artisan make:migration create_table_organizacao --path=database/migrations/admin
-php artisan make:migration create_table_usuario --path=database/migrations/admin
-php artisan make:migration create_table_pessoa --path=database/migrations/admin
+php artisan make:migration create_table_uf --create=uf --path=database/migrations/admin
+php artisan make:migration create_table_organizacao --create=organizacao --path=database/migrations/admin
+php artisan make:migration create_table_usuario --create=usuario --path=database/migrations/admin
+php artisan make:migration create_table_pessoa --create=pessoa --path=database/migrations/admin
 
 
-
-
-
-
-php artisan migrate --path=/database/migrations/2022_03_23_165209_create_uf_table.php
-php artisan migrate --path=/database/migrations/2022_03_23_163500_create_organizacao_table.php
-php artisan migrate --path=/database/migrations/2022_03_23_195213_create_usuario_table.php
-php artisan migrate --path=/database/migrations/2022_03_23_194648_create_pessoa_table.php
-php artisan migrate --path=/database/migrations/2022_03_23_195744_create_regional_table.php
-
-
-
+# Execução da Migration para criação das tabelas
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_uf.php
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_organizacao.ph
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_usuario.php
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_pessoa.php
 
 
 
