41e14952242a6be0451281b422729d0681be3aa6
jshenriquedf <68820356+jshenriquedf@users.noreply.github.com>
2022-03-23 23:25:21 -0300

    Update Ordem de execução.sh

--- Sistema Processo PHP/Migration/Ordem de execução.sh
+++ Sistema Processo PHP/Migration/Ordem de execução.sh
@@ -3,13 +3,16 @@
 # Criação de arquivos migration
 php artisan make:migration create_table_uf --create=uf --path=database/migrations/admin
 php artisan make:migration create_table_organizacao --create=organizacao --path=database/migrations/admin
+php artisan make:migration create_table_perfil --create=perfil --path=database/migrations/admin
 php artisan make:migration create_table_usuario --create=usuario --path=database/migrations/admin
 php artisan make:migration create_table_pessoa --create=pessoa --path=database/migrations/admin
 
 
+
 # Execução da Migration para criação das tabelas
 php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_uf.php
-php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_organizacao.ph
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_organizacao.php
+php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_perfil.php
 php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_usuario.php
 php artisan migrate --path=/database/migrations/admin/2022_03_24_000000_create_table_pessoa.php
 
